08817cba52cbdafef03b1f18c3ff9e7ffed35b96
AESS150715 <albertsutanto9@gmail.com>
2020-01-08 22:18:01 +0700

    updated_22:17_1/8/2019

---
+++ WebProgrammingPracticumFinal/database/migrations/2019_12_11_074159_create_feedbacks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->bigInteger('id')->unsigned();
+            $table->foreign('id')->references('id')->on('users');
+            $table->bigIncrements('feed_id');
+            $table->string('status');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+}
